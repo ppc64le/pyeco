239de3da5030bb2e2de7c5632bf0447f5352729a
atongse <aditya.tongse@ibm.com>
2025-09-02 17:53:42 +0530

    Added differentiation based on OS

--- pytorch-example/run_pytorch.sh
+++ pytorch-example/run_pytorch.sh
@@ -1,5 +1,46 @@
-yum install gcc-toolset-13 python3.11-devel python3.11-pip -y
-source /opt/rh/gcc-toolset-13/enable
+#!/bin/bash
+
+# Function to detect Linux distribution
+detect_distro() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        echo $ID
+    elif [ -f /etc/redhat-release ]; then
+        echo "rhel"
+    elif [ -f /etc/debian_version ]; then
+        echo "debian"
+    else
+        echo "unknown"
+    fi
+}
+
+# Install system dependencies based on distribution
+DISTRO=$(detect_distro)
+
+case $DISTRO in
+    "fedora"|"rhel"|"centos"|"rocky"|"almalinux")
+        if command -v dnf >/dev/null 2>&1; then
+            sudo dnf install gcc-toolset-13 python3.11-devel python3.11-pip -y --skip-broken --nobest
+            source /opt/rh/gcc-toolset-13/enable
+        else
+            sudo yum install gcc-toolset-13 python3.11-devel python3.11-pip -y
+            source /opt/rh/gcc-toolset-13/enable
+        fi
+        ;;
+    "ubuntu"|"debian")
+        # Use: bash script.sh
+        apt update -y
+        apt install -y software-properties-common curl lsb-release gnupg2  libgomp1
+
+        add-apt-repository ppa:deadsnakes/ppa -y
+        apt update -y
+        apt install -y python3.11 python3.11-venv python3.11-distutils libgfortran5 gcc-13
+        ;;
+    *)
+        echo "Unsupported distribution: $DISTRO"
+        exit 1
+        ;;
+esac
 
 python3.11 -m venv venv
 source venv/bin/activate
@@ -13,7 +54,7 @@ export LD_LIBRARY_PATH=./venv/lib64/python3.11/site-packages/openblas/lib:$LD_LI
 
 python pytorch_example.py
 
-echo "==== Running tests ===="
+echo "\n ==== Running tests ==== \n"
 
 python sub-test1.py
 python sub-test2.py
