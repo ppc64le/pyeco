52bd6298cfd93afa7d7e664f3ee6f477596605f5
atongse <aditya.tongse@ibm.com>
2025-09-02 20:50:19 +0530

    updated run_vllm.sh file

--- vllm-example/run_vllm.sh
+++ vllm-example/run_vllm.sh
@@ -1,9 +1,51 @@
-yum install gcc-toolset-13 python3.12-devel python3.12-pip libjpeg-turbo-devel numactl gcc gcc-c++ gcc-gfortran xz cmake yum-utils \
-    openssl-devel openblas-devel bzip2-devel bzip2 libffi-devel \
-    zlib-devel autoconf automake libtool cargo \
-    pkgconf-pkg-config fontconfig fontconfig-devel sqlite-devel -y
+#!/bin/bash
 
-source /opt/rh/gcc-toolset-13/enable
+# Function to detect Linux distribution
+detect_distro() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        echo $ID
+    elif [ -f /etc/redhat-release ]; then
+        echo "rhel"
+    elif [ -f /etc/debian_version ]; then
+        echo "debian"
+    else
+        echo "unknown"
+    fi
+}
+
+# Install system dependencies based on distribution
+DISTRO=$(detect_distro)
+
+case $DISTRO in
+    "fedora"|"rhel"|"centos"|"rocky"|"almalinux")
+        if command -v dnf >/dev/null 2>&1; then
+            dnf install gcc-toolset-13 python3.12-devel python3.12-pip libjpeg-turbo-devel numactl gcc gcc-c++ gcc-gfortran xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 libffi-devel \
+            zlib-devel autoconf automake libtool cargo \
+            pkgconf-pkg-config fontconfig fontconfig-devel sqlite-devel -y --skip-broken --nobest
+            
+            source /opt/rh/gcc-toolset-13/enable
+        else
+            yum install gcc-toolset-13 python3.12-devel python3.12-pip libjpeg-turbo-devel numactl gcc gcc-c++ gcc-gfortran xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 libffi-devel \
+            zlib-devel autoconf automake libtool cargo \
+            pkgconf-pkg-config fontconfig fontconfig-devel sqlite-devel -y
+            source /opt/rh/gcc-toolset-13/enable
+        fi
+        ;;
+    "ubuntu"|"debian")
+        # Use: bash script.sh
+        apt update &&  apt install -y \
+        gcc g++ gfortran python3.12 python3.12-dev python3.12-venv python3-pip \
+        libjpeg-turbo8-dev libnuma-dev \
+        xz-utils cmake libssl-dev libopenblas-dev \
+        libbz2-dev libbz2-1.0 libffi-dev zlib1g-dev autoconf automake libtool \
+        cargo pkg-config fontconfig libfontconfig1-dev sqlite3 libsqlite3-dev libjpeg62
+        ;;
+    *)
+        echo "Unsupported distribution: $DISTRO"
+        exit 1
+        ;;
+esac
 
 python3.12 -m venv venv
 source venv/bin/activate
