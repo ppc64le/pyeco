6a87a43af33a68e10a48764e365aabfb16c63737
Vipul Ajmera <Vipul.Ajmera@ibm.com>
2025-09-02 23:43:02 +0530

    Update example2.py

--- examples/Example2/example2.py
+++ examples/Example2/example2.py
@@ -9,7 +9,16 @@ import os
 nb = nbf.v4.new_notebook()
 nb.cells = [
     nbf.v4.new_markdown_cell("# Auto Notebook\nThis was generated and executed programmatically."),
-    nbf.v4.new_code_cell("import numpy as np\nimport matplotlib.pyplot as plt\nx=np.linspace(0,6.28,300)\ny=np.sin(x)\nplt.plot(x,y)\nplt.title('Sine')\nplt.savefig('nb_plot.png',dpi=120)\nprint('sum(y)=',y.sum())")
+    nbf.v4.new_code_cell(
+        "import numpy as np\n"
+        "import matplotlib.pyplot as plt\n"
+        "x = np.linspace(0, 6.28, 300)\n"
+        "y = np.sin(x)\n"
+        "plt.plot(x, y)\n"
+        "plt.title('Sine')\n"
+        "plt.savefig('nb_plot.png', dpi=120)\n"
+        "print('sum(y)=', y.sum())"
+    )
 ]
 
 # Execute with ipykernel
@@ -20,8 +29,8 @@ ep.preprocess(nb, {})
 with open("auto_notebook.ipynb", "w", encoding="utf-8") as f:
     nbf.write(nb, f)
 
-# Export to HTML
-exporter = HTMLExporter()
+# Export to HTML 
+exporter = HTMLExporter(template_name='basic')
 (body, resources) = exporter.from_notebook_node(nb)
 with open("auto_notebook.html", "w", encoding="utf-8") as f:
     f.write(body)
