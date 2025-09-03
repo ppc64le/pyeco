3d876ea5df7eed84df8827bf5bc6c7e52380cc42
JeSh30 <rajeshanumala1230@gmail.com>
2025-09-03 19:13:33 +0530

    Adding examples for ONNX packages

--- onnx_example/run_onnx.sh
+++ onnx_example/run_onnx.sh
@@ -52,7 +52,7 @@ WORKDIR=$(pwd)
 export LD_LIBRARY_PATH="./venv/lib/python3.12/site-packages/openblas/lib:$LD_LIBRARY_PATH"
 cd ./venv/lib/python3.12/site-packages/libprotobuf/lib64/
 ln -s libprotobuf.so.25.4.0 libprotobuf.so.25.3.0
-
+export LD_LIBRARY_PATH="./venv/lib/python3.12/site-packages/libprotobuf/lib64:$LD_LIBRARY_PATH"
 cd $WORKDIR
 
 python3.12 onnx_example.py
