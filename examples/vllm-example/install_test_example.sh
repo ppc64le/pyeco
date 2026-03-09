d35ff1c71bd722b1c85c73f493debca1cdacf8e9
Aditya-Tongse <Aditya.Tongse@ibm.com>
2026-03-09 16:12:34 +0530

    noninteractive frontend for vllm

--- examples/vllm-example/install_test_example.sh
+++ examples/vllm-example/install_test_example.sh
@@ -34,6 +34,7 @@ case $DISTRO in
         ;;
     "ubuntu"|"debian")
         # Use: bash script.sh
+        export DEBIAN_FRONTEND=noninteractive
         sudo apt update &&  sudo apt install -y \
         gcc g++ gfortran python3.12 python3.12-dev python3.12-venv python3-pip \
         libjpeg-turbo8-dev libnuma-dev \
