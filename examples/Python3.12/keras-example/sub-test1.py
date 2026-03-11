8b0d0c40b9c69ba36e183acaaf7a3dc49632ec9c
Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
2026-03-11 07:51:28 +0000

    updated folder structure

--- examples/Python3.12/keras-example/sub-test1.py
+++
@@ -1,6 +0,0 @@
-import keras
-print("===== SUBTEST 1: Architecture Validation =====")
-model = keras.models.load_model("trained_model.h5")
-layer_count = len(model.layers)
-print("Layer count:", layer_count)
-assert layer_count >= 6, "Model architecture too shallow"
